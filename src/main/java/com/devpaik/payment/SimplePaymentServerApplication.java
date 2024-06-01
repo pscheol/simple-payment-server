c73d06ebb8b3b09a9a3773cb090b4f27056a72c5
pscheol <pscheol@gmail.com>
2024-06-02 01:48:20 +0900

    Initial project

---
+++ src/main/java/com/devpaik/payment/SimplePaymentServerApplication.java
@@ -0,0 +1,13 @@
+package com.devpaik.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePaymentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePaymentServerApplication.class, args);
+    }
+
+}
