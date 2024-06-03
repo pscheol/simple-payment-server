e5d44a5dffc3fff5f13660a46ee7b31a13fd9e54
pscheol <pscheol@gmail.com>
2024-06-03 21:01:21 +0900

    feat : 테스트쿼리 추가 import.sql, tb_daily_exchange_rate

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,14 @@
 insert into tb_currency(currency_code, currency_name) values('KRW', '원');
 insert into tb_currency(currency_code, currency_name) values('USD', '달러');
 
+insert into tb_daily_exchange_rate(currency_code, exchange_rate, current_dtm) values('USD', 1482.52, now());
+
+
 insert into tb_user(user_id, name) values('user1234', '홍길동');
 
 insert into tb_wallet(wallet_id, user_id, currency_code, balnace) values('wallet1','user1234', 'USD', 1000.25);
-insert into tb_wallet(wallet_id, user_id, currency_code, balnace) values('wallet2','user1234', 'KRW', 0);
+insert into tb_wallet(wallet_id, user_id, currency_code, balnace) values('wallet2','user1234', 'KRW', 10000);
 
 insert into tb_merchant(merchant_id, merchant_name) values('merchantId1234', '테스트상점');
+
 
